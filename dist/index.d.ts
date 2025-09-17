df0a78a0dae4c1923b674f083441a887f025a82c
Koen van Esterik <koen@vanesterik.com>
2025-09-17 15:46:58 +0100

    fix: export input component part 3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,5 +2,6 @@ export { Button } from './components/Button/Button';
 export { Card } from './components/Card/Card';
 export { Icon } from './components/Icon/Icon';
 export { Input } from './components/Input/Input';
+export { Kbd } from './components/Kbd/Kbd';
 export { Text } from './components/Text/Text';
 //# sourceMappingURL=index.d.ts.map
